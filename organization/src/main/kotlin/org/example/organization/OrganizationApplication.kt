36c4a8f044acd3dbe0ca52a8aa528c4b60f8fd09
Javokhirkh <javohir23.09.2003@gmail.com>
2026-02-04 15:06:44 +0500

    add Project , Organization, Task services

---
+++ organization/src/main/kotlin/org/example/organization/OrganizationApplication.kt
@@ -0,0 +1,11 @@
+package org.example.organization
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OrganizationApplication
+
+fun main(args: Array<String>) {
+    runApplication<OrganizationApplication>(*args)
+}
