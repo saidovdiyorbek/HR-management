e5362d43543a7cf69d196d4e981fea504bd515ca
HP <saidovdiyer777@gmail.com>
2026-02-06 15:40:39 +0500

    feat(Task): user role constants

--- task/src/main/kotlin/org/example/task/Enums.kt
+++ task/src/main/kotlin/org/example/task/Enums.kt
@@ -11,3 +11,9 @@ enum class TaskPriority{
 enum class ErrorCode(val code: Int, val message: String) {
     TASK_NOT_FOUND(100, "TASK_NOT_FOUND"),
 }
+
+enum class Role {
+    USER,
+    ADMIN,
+    DEVELOPER,
+}
