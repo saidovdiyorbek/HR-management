ebe5fa34f8baf645cc171921008312240f24da8f
HP <saidovdiyer777@gmail.com>
2026-02-06 17:47:56 +0500

    feat(Task): added util generate hash

--- task/src/main/kotlin/org/example/task/Utils.kt
+++ task/src/main/kotlin/org/example/task/Utils.kt
@@ -28,3 +28,14 @@ class SecurityUtil{
     }
 
 }
+
+@Component
+class GenerateHash(){
+    fun generateHash(): String{
+        val charPool : List<Char> = ('a'..'z') + ('A'..'Z') + ('0'..'9')
+        val randomString = (1..10)
+            .map { i -> kotlin.random.Random.nextInt(0, charPool.size).let { charPool[it] } }
+            .joinToString("")
+        return randomString
+    }
+}
