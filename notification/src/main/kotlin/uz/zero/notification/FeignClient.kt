6e32d46f981d476fc3777f59afbfe2ab612572d0
HP <saidovdiyer777@gmail.com>
2026-02-18 14:05:54 +0500

    feat(Notification): added feign client

--- notification/src/main/kotlin/uz/zero/notification/FeignClient.kt
+++ notification/src/main/kotlin/uz/zero/notification/FeignClient.kt
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import uz.zero.notification.dtos.CurrentOrganizationResponse
+import uz.zero.notification.dtos.OrganizationInfo
 import uz.zero.notification.dtos.SendMessageRequest
 import uz.zero.notification.dtos.UserShortInfo
 
@@ -24,4 +26,20 @@ interface AuthUserClient {
     @GetMapping("user-short-info/{userId}")
     fun getUserShortInfo(@PathVariable userId: Long): UserShortInfo
 
+}
+
+@FeignClient(name = "employee-service", url = "\${services.hosts.organization}/internal/employee-context", configuration = [FeignOAuth2TokenConfig::class])
+interface EmployeeContextClient {
+
+    @GetMapping("/get-current-organization/{userId}")
+    fun getCurrentOrganizationByUserId(
+        @PathVariable userId: Long
+    ): CurrentOrganizationResponse
+}
+
+@FeignClient(name = "organization-service", url = "\${services.hosts.organization}/internal/api/v1/organizations", configuration = [FeignOAuth2TokenConfig::class])
+interface OrganizationClient {
+
+    @GetMapping("/get-organization-info/{userId}")
+    fun getOrganizationInfo(@PathVariable userId: Long): OrganizationInfo
 }
