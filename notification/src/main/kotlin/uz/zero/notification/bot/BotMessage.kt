7a3d4d90f406ff5030f05160cbd354af736c2338
khondamir <xondamirmusaxonov212@gmail.com>
2026-02-18 16:34:34 +0500

    add:BotMessage

---
+++ notification/src/main/kotlin/uz/zero/notification/bot/BotMessage.kt
@@ -0,0 +1,48 @@
+package uz.zero.notification.bot
+
+import java.time.LocalDateTime
+
+class BotMessage(
+
+) {
+    fun create( date: LocalDateTime,
+               organizationName: String,
+               projectName: String,
+               actionOwner: String,
+               title: String,
+               taskUrl: String): String {
+        return """
+            📋 <b>Topshiriq yaratildi:</b>
+            
+            🕐 $date
+            🏢 Tashkilot nomi: $organizationName
+            📁 Loyiha nomi: $projectName
+            🧑‍💼 Harakat egasi: $actionOwner
+            📝 Sarlavha: $title
+            📊 Holat: Task yaratildi
+            🔗 <a href="$taskUrl">Topshiriqni ochish</a>
+        """.trimIndent()
+    }
+
+    fun changeTaskState(date: LocalDateTime,
+                        organizationName: String,
+                        projectName: String,
+                        actionOwner: String,
+                        title: String,
+                        fromState: String,
+                        toState: String,
+                        taskUrl: String) : String {
+
+        return """
+            📋 <b>Topshiriqning holati o'zgartirildi:</b>
+            
+            🕐 $date
+            🏢 Tashkilot nomi: $organizationName
+            📁 Loyiha nomi: $projectName
+            🧑‍💼 Harakat egasi: $actionOwner
+            📝 Sarlavha: $title
+            📊 Holat: $fromState >> $toState
+            🔗 <a href="$taskUrl">Topshiriqni ochish</a>
+        """.trimIndent()
+    }
+}
