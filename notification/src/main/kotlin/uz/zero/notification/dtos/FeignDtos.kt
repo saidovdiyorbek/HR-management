027e1339acf1383b8499fd7eebeb1fdae27a5b6f
HP <saidovdiyer777@gmail.com>
2026-02-18 14:06:39 +0500

    feat(Notification): full name

--- notification/src/main/kotlin/uz/zero/notification/dtos/FeignDtos.kt
+++ notification/src/main/kotlin/uz/zero/notification/dtos/FeignDtos.kt
@@ -6,6 +6,7 @@ data class UserShortInfo(
     val userId: Long,
     val telegramChatId: Long? = null,
     val username: String,
+    val fullName: String,
 )
 
 data class TaskActionCreateDto(
@@ -13,4 +14,16 @@ data class TaskActionCreateDto(
     val userId: Long,
     val type: ActionType,
     val details: String? = null,
+)
+
+data class CurrentOrganizationResponse(
+    val organizationId: Long,
+    val employeeId: Long,
+    val userId: Long,
+)
+
+data class OrganizationInfo(
+    val id: Long,
+    val name: String,
+    val description: String? = null,
 )
