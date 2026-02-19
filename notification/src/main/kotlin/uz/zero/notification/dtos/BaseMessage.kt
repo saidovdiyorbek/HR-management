6c9f42906054da8c3d091b9c1e32c0ad1b591e2d
Javokhirkh <javohir23.09.2003@gmail.com>
2026-02-19 16:29:36 +0500

    fix(notification) remove and make all code in one file

--- notification/src/main/kotlin/uz/zero/notification/dtos/BaseMessage.kt
+++
@@ -1,7 +0,0 @@
-package uz.zero.notification.dtos
-
-data class BaseMessage(val code: Int?, val message: String? = null){
-    companion object{
-        var OK = BaseMessage(code = 0, message = "OK")
-    }
-}
